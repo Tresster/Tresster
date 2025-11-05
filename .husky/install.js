b7143639aac05c83d22b63431a038a6c8c5537bb
Tresster <25589237+Tresster@users.noreply.github.com>
2025-11-05 15:53:31 +0100

    removed husky install.js.

--- .husky/install.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-if (process.env['NODE_ENV'] === 'production' || process.env['CI'] === 'true') process.exit(0);
-
-const husky = (await import('husky')).default;
-
-const husky_ = husky();
-if (husky_ !== '') console.log(husky_);
